9145abf6b42c40eaff62bcd9b7b7049563850253
jjuiddong <jjuiddong@gmail.com>
2019-04-07 22:09:12 +0900

    update refactoring ui

--- Src/MapEditor/mapeditor.cpp
+++ Src/MapEditor/mapeditor.cpp
@@ -60,78 +60,7 @@ bool cMapEditor::OnInit()
 	const int cy = GetSystemMetrics(SM_CYSCREEN);
 
 	m_gui.SetContext();
-
-	float col_main_hue = 0.0f / 255.0f;
-	float col_main_sat = 0.0f / 255.0f;
-	float col_main_val = 80.0f / 255.0f;
-
-	float col_area_hue = 0.0f / 255.0f;
-	float col_area_sat = 0.0f / 255.0f;
-	float col_area_val = 50.0f / 255.0f;
-
-	float col_back_hue = 0.0f / 255.0f;
-	float col_back_sat = 0.0f / 255.0f;
-	float col_back_val = 35.0f / 255.0f;
-
-	float col_text_hue = 0.0f / 255.0f;
-	float col_text_sat = 0.0f / 255.0f;
-	float col_text_val = 255.0f / 255.0f;
-
-	float col_button_hue = 0.0f / 255.0f;
-	float col_button_sat = 0.0f / 255.0f;
-	float col_button_val = 120.0f / 255.0f;
-
-	float frameRounding = 0.0f;
-
-	ImVec4 col_text = ImColor::HSV(col_text_hue, col_text_sat, col_text_val);
-	ImVec4 col_main = ImColor::HSV(col_main_hue, col_main_sat, col_main_val);
-	ImVec4 col_button = ImColor::HSV(col_button_hue, col_button_sat, col_button_val);
-	ImVec4 col_area = ImColor::HSV(col_area_hue, col_area_sat, col_area_val);
-	ImVec4 col_back = ImColor::HSV(col_back_hue, col_back_sat, col_back_val);
-	float rounding = frameRounding;
-
-	ImGuiStyle& style = ImGui::GetStyle();
-	style.FrameRounding = rounding;
-	style.WindowRounding = rounding;
-	style.Colors[ImGuiCol_Text] = ImVec4(col_text.x, col_text.y, col_text.z, 1.00f);
-	style.Colors[ImGuiCol_TextDisabled] = ImVec4(col_text.x, col_text.y, col_text.z, 0.58f);
-	style.Colors[ImGuiCol_WindowBg] = ImVec4(col_back.x, col_back.y, col_back.z, 0.80f);
-	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
-	style.Colors[ImGuiCol_PopupBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
-	style.Colors[ImGuiCol_Border] = ImVec4(col_text.x, col_text.y, col_text.z, 0.30f);
-	style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-	style.Colors[ImGuiCol_FrameBg] = ImVec4(col_back.x, col_back.y, col_back.z, 1.00f);
-	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.68f);
-	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_TitleBg] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
-	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
-	style.Colors[ImGuiCol_TitleBgActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
-	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.0f);
-	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
-	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(col_main.x, col_main.y, col_main.z, 0.31f);
-	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.78f);
-	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_CheckMark] = ImVec4(col_text.x, col_text.y, col_text.z, 0.80f);
-	style.Colors[ImGuiCol_SliderGrab] = ImVec4(col_main.x, col_main.y, col_main.z, 0.54f);
-	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_Button] = ImVec4(col_button.x, col_button.y, col_button.z, 0.44f);
-	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(col_button.x, col_button.y, col_button.z, 0.86f);
-	style.Colors[ImGuiCol_ButtonActive] = ImVec4(col_button.x, col_button.y, col_button.z, 1.00f);
-	style.Colors[ImGuiCol_Header] = ImVec4(col_main.x, col_main.y, col_main.z, 0.76f);
-	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.86f);
-	style.Colors[ImGuiCol_HeaderActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_Column] = ImVec4(col_text.x, col_text.y, col_text.z, 0.32f);
-	style.Colors[ImGuiCol_ColumnHovered] = ImVec4(col_text.x, col_text.y, col_text.z, 0.78f);
-	style.Colors[ImGuiCol_ColumnActive] = ImVec4(col_text.x, col_text.y, col_text.z, 1.00f);
-	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(col_main.x, col_main.y, col_main.z, 0.20f);
-	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.78f);
-	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_PlotLines] = ImVec4(col_text.x, col_text.y, col_text.z, 0.63f);
-	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_PlotHistogram] = ImVec4(col_text.x, col_text.y, col_text.z, 0.63f);
-	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
-	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(col_main.x, col_main.y, col_main.z, 0.43f);
-	style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.10f, 0.10f, 0.10f, 0.55f);
+	m_gui.SetStyleColorsDark();
 
 	return true;
 }
